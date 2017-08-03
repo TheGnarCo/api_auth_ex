958d67c3fc5309a4986c7a0213f51ce9dd456928
zfletch <zfletch2@gmail.com>
2017-08-03 15:33:04 -0400

    generate all http headers required
    The `sign!` method in
    [api_auth](https://github.com/mgomes/api_auth/blob/4ea2e2f6abed1fdf650ec716ca86c2ed9f640b45/spec/api_auth_spec.rb#L26)
    adds more than just the `Authorization` header. It also adds the
    following headers:
    
    * `DATE`
    * `Content-MD5`
    
    This commit adds those headers in a way that should be compatible with
    `api_auth`. It replaces the `authorization` method with `headers`, and
    instead of returning a string, returns a map of all of the headers that
    should be included in the request in order to be authenticated by
    `api_auth`.
    
    It also makes a few other small changes to match the behavior of
    `api_auth`

--- test/api_auth_test.exs
+++ test/api_auth_test.exs
@@ -2,32 +2,95 @@ defmodule ApiAuthTest do
   use ExUnit.Case
   doctest ApiAuth
 
-  test "calculates signature" do
-    authorization = ApiAuth.authorization(
-      "/",
-      "1044",
-      "123",
-      hashed_content: "",
-      time: time(),
-      signature_hash: :sha,
-    )
+  describe "Date header" do
+    test "adds the date to the header" do
+      %{ "DATE" => date } = ApiAuth.headers("/", "1044", "123", time: time())
 
-    assert authorization == "APIAuth 1044:49FglhLqXWuJqBu5SQOH4F8D1Og="
+      assert date == "Sat, 01 Jan 2000 00:00:00 GMT"
+    end
+
+    test "uses the current time if none is given" do
+      %{ "DATE" => date } = ApiAuth.headers("/", "1044", "123")
+
+      diff = Calendar.DateTime.now_utc()
+             |> DateTime.diff(Calendar.DateTime.Parse.httpdate!(date), :second)
+
+      assert diff == 0
+    end
   end
 
-  test "calculates signature with method, content type, and body" do
-    authorization = ApiAuth.authorization(
-      "/resource.xml?foo=bar&bar=foo",
-      "1044",
-      "123",
-      method: "PUT",
-      content_type: "text/plain",
-      time: time(),
-      content_hash: :md5,
-      signature_hash: :sha256,
-    )
+  describe "Content hash header" do
+    test "there is no content hash when it is not a POST or PUSH" do
+      headers = ApiAuth.headers("/", "1044", "123")
 
-    assert authorization == "APIAuth 1044:5JhErRhsIbN2+O595t/Rkax2n7w/YZ0f92BYgZFN5ds="
+      assert headers["X-APIAuth-Content-Hash"] == nil
+    end
+
+    test "the content hash is calculated correctly" do
+      %{ "X-APIAuth-Content-Hash" => hash } = ApiAuth.headers(
+        "/",
+        "1044",
+        "123",
+        method: "POST",
+        content: "foo",
+      )
+
+      assert hash == "LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564="
+    end
+
+    test "the content hash header is different for md5" do
+      %{ "Content-MD5" => hash } = ApiAuth.headers(
+        "/",
+        "1044",
+        "123",
+        method: "POST",
+        content: "foo",
+        content_algorithm: :md5,
+      )
+
+      assert hash == "rL0Y20zC+Fzt72VPzMSk2A=="
+    end
+
+    test "the content hash can be overwritten" do
+      %{ "X-APIAuth-Content-Hash" => hash } = ApiAuth.headers(
+        "/",
+        "1044",
+        "123",
+        method: "POST",
+        content: "foo",
+        content_hash: "pre-computed hash",
+      )
+
+      assert hash == "pre-computed hash"
+    end
+  end
+
+  describe "Authorization header" do
+    test "calculates signature" do
+      %{ "Authorization" => authorization } = ApiAuth.headers(
+        "/",
+        "1044",
+        "123",
+        time: time(),
+        signature_algorithm: :sha,
+      )
+
+      assert authorization == "APIAuth 1044:49FglhLqXWuJqBu5SQOH4F8D1Og="
+    end
+
+    test "calculates signature with method, content type, and body" do
+      %{ "Authorization" => authorization } = ApiAuth.headers(
+        "/resource.xml?foo=bar&bar=foo",
+        "1044",
+        "123",
+        method: "PUT",
+        content_type: "text/plain",
+        time: time(),
+        content_algorithm: :md5,
+      )
+
+      assert authorization == "APIAuth-HMAC-SHA256 1044:5JhErRhsIbN2+O595t/Rkax2n7w/YZ0f92BYgZFN5ds="
+    end
   end
 
   defp time do
