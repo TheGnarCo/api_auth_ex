e79c565a89383ce7e321724cd1dc78f4d7cc8b9f
zfletch <zfletch2@gmail.com>
2017-08-03 09:20:54 -0400

    authorization header
    This is the first step to creating an Elixir library that can sign API
    requests using Amazon-style HMAC authentication and also interface with
    Ruby/Rails applications that use
    [api_auth](https://github.com/mgomes/api_auth).
    
    It does not represent the final state of this library's API.
    
    What this commit does is:
    
    * Take some tests from
      [api_auth](https://github.com/mgomes/api_auth/blob/4ea2e2f6abed1fdf650ec716ca86c2ed9f640b45/spec/api_auth_spec.rb#L54)
      and find what the value of the `Authorization` header should be for a
      particular time
    * Create Elixir versions of those tests
    * Write a function that generates a matching `Authorization` header
      given the correct input

--- lib/api_auth.ex
+++ lib/api_auth.ex
@@ -1,18 +1,69 @@
 defmodule ApiAuth do
   @moduledoc """
-  Documentation for ApiAuth.
+  This is the ApiAuth module.
+
+  It provides an HMAC authentication system for APIs.
   """
 
+  alias Calendar.DateTime, as: DateTime
+
   @doc """
-  Hello world.
+  Generates an Authorization header
+
+  Returns the string "ApiAuth client_id:<signature>"
 
   ## Examples
 
-      iex> ApiAuth.hello
-      :world
+      iex> ApiAuth.authorization("/foo", "id", "secret", method: "GET",
+      ...>                       time: "Sat, 01 Jan 2000 00:00:00 GMT" |> Calendar.DateTime.Parse.httpdate!)
+      "APIAuth id:Uv9YsGct6RFBFDxn5NhUPGqe+EVRQXrE5QLjVWXkga8="
 
   """
-  def hello do
-    :world
+  def authorization(uri, client_id, secret_key, opts \\ []) do
+    method         = opts |> Keyword.get(:method, "GET")
+    content_type   = opts |> Keyword.get(:content_type, "")
+    content        = opts |> Keyword.get(:content, "")
+    content_hash   = opts |> Keyword.get(:content_hash, :sha256)
+    hashed_content = opts |> Keyword.get(:hashed_content, hashed(content, content_hash))
+    time           = opts |> Keyword.get(:time, DateTime.now_utc)
+    separator      = opts |> Keyword.get(:separator, ",")
+    signature_hash = opts |> Keyword.get(:signature_hash, :sha256)
+
+    string = canonical_string(
+      String.upcase(method),
+      content_type,
+      hashed_content,
+      uri,
+      timestamp(time),
+      separator
+    )
+
+    string
+    |> signature(secret_key, signature_hash)
+    |> header(client_id)
+  end
+
+  defp header(signature, client_id) do
+    "APIAuth #{client_id}:#{signature}"
+  end
+
+  defp timestamp(time) do
+    time
+    |> DateTime.Format.httpdate
+  end
+
+  defp hashed(content, hash) do
+    :crypto.hash(hash, content)
+    |> Base.encode64()
+  end
+
+  defp canonical_string(method, content_type, hashed_content, request_uri, timestamp, separator) do
+    [method, content_type, hashed_content, request_uri, timestamp]
+    |> Enum.join(separator)
+  end
+
+  defp signature(canonical_string, secret_key, hash) do
+    :crypto.hmac(hash, secret_key, canonical_string)
+    |> Base.encode64()
   end
 end
--- test/api_auth_test.exs
+++ test/api_auth_test.exs
@@ -2,7 +2,35 @@ defmodule ApiAuthTest do
   use ExUnit.Case
   doctest ApiAuth
 
-  test "greets the world" do
-    assert ApiAuth.hello() == :world
+  test "calculates signature" do
+    authorization = ApiAuth.authorization(
+      "/",
+      "1044",
+      "123",
+      hashed_content: "",
+      time: time(),
+      signature_hash: :sha,
+    )
+
+    assert authorization == "APIAuth 1044:49FglhLqXWuJqBu5SQOH4F8D1Og="
+  end
+
+  test "calculates signature with method, content type, and body" do
+    authorization = ApiAuth.authorization(
+      "/resource.xml?foo=bar&bar=foo",
+      "1044",
+      "123",
+      method: "PUT",
+      content_type: "text/plain",
+      time: time(),
+      content_hash: :md5,
+      signature_hash: :sha256,
+    )
+
+    assert authorization == "APIAuth 1044:5JhErRhsIbN2+O595t/Rkax2n7w/YZ0f92BYgZFN5ds="
+  end
+
+  defp time do
+    "Sat, 01 Jan 2000 00:00:00 GMT" |> Calendar.DateTime.Parse.httpdate!
   end
 end
